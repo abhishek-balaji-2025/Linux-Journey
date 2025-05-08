9614a71f7919924c147a24d693e78ff3958e8160
Abhishek-Balaji <abhishekbalajico@gmail.com>
2025-05-08 13:47:56 +0530

    another simple-if-condition example using square brackets and flags

---
+++ simple-if-condition-2.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Author: Abhishek Balaji
+# date of creation of bash script: 13:42 / 08-05-2025
+echo "View code for better understanding"
+
+a=5
+
+if [ "$a" -gt 2 ]
+then
+  echo "yes, $a is greater than 2"
+fi
+
+# Another example
+
+b=32
+
+if [ "$b" -le 80 ]
+then
+  echo "$b is less than 80"
+fi
+
+if [ "$b" -le 32 ]
+then 
+  echo "$b is equal to 32"
+fi
+
+
