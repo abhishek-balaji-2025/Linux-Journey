cc7f061ea3362348ece8efaa43f4c66a6a9d97e5
Abhishek-Balaji <abhishekbalajico@gmail.com>
2025-05-11 12:07:46 +0530

    while-condition example-1 is added

---
+++ while-condition.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Print numbers from 1-10
+
+i=1
+while (( i < 11 ))
+do
+  echo $i
+  (( i = i + 1 ))
+done
+
+# Keynote
+# Always remember any arithmatic operation needs to be present within the double parenthesis (( ))
+
+
+
